bdb5f41ccb1c2a970eae962d21cf6e9a5889ee04
Sarah DELMAS <s.delmas@nanocorp.fr>
2023-05-05 12:08:42 +0200

    remove useless variables

--- degres_convertor/src/main.rs
+++ degres_convertor/src/main.rs
@@ -4,26 +4,9 @@ fn main() {
     // 1. l'utilisateur choisit vers quelle unité convertir sa témperature
     println!("Do you want to convert to Celsius or Fahrenheit? Input C or F");
 
-    let mut convert_type = String::new();
-    io::stdin()
-        .read_line(&mut convert_type)
-        .expect("Enter a value!");
-
-    let unit: &str = match convert_type.trim() {
-        "F" => {
-            println!("You choose fahrenheit");
-            "F"
-        }
-        "C" => {
-            println!("You choose Celsius");
-            "C"
-        }
-        _ => {
-            println!("Wrong value e_e");
-            return;
-        }
-    };
-
+    let mut unit = String::new();
+    io::stdin().read_line(&mut unit).expect("Enter a value!");
+    let unit: &str = unit.trim();
     let contrary_unit = if unit == "C" { "F" } else { "C" };
 
     // 2. l'utilisateur entre une température à convertir
@@ -34,14 +17,12 @@ fn main() {
         .read_line(&mut value_to_convert)
         .expect("Enter a valid value");
 
-    let value_to_convert: f32 = value_to_convert.trim().parse().unwrap();
-
-    let converted_value: f32 = match unit {
-      "F" => (value_to_convert -32.0) / 1.8,
-      "C" => (value_to_convert * 1.8) + 32.0,
-      _ => return
-    };
-
     // 3. nous retournons la température convertie
+    let value_to_convert: f32 = value_to_convert.trim().parse().unwrap();
+    let converted_value: f32 = match unit {
+        "F" => (value_to_convert - 32.0) / 1.8,
+        "C" => (value_to_convert * 1.8) + 32.0,
+        _ => return,
+    };
     println!("{value_to_convert}°{unit} is equal to {converted_value:.2}°{contrary_unit}")
 }
