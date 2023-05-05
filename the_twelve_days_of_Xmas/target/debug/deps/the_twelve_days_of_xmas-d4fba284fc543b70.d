b5578626d3fc5fc9532247a238a6f2b2a0e0b0be
Sarah DELMAS <s.delmas@nanocorp.fr>
2023-05-05 13:31:36 +0200

    commit all

---
+++ the_twelve_days_of_Xmas/target/debug/deps/the_twelve_days_of_xmas-d4fba284fc543b70.d
@@ -0,0 +1,5 @@
+C:\Users\SarahDELMAS\Documents\Tests\rust_playground\the_twelve_days_of_xmas\target\debug\deps\the_twelve_days_of_xmas-d4fba284fc543b70.rmeta: src\main.rs
+
+C:\Users\SarahDELMAS\Documents\Tests\rust_playground\the_twelve_days_of_xmas\target\debug\deps\the_twelve_days_of_xmas-d4fba284fc543b70.d: src\main.rs
+
+src\main.rs:
